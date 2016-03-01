80489fe41fc1a3863e2713d2ec533c99713f5ae3
Matthew.Ma <matthew.ma@jnsolutions.com.au>
2016-03-02 00:31:11 +1100

    simple routing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-var App = console.log('Hello World from Browerserify');
+(function (win) {
+"use strict";
+var App = React.createClass({
+	render: function(){
+		var Child;
+		switch(this.props.route){
+			case 'about': Child = About;
+			break;
+			default: Child = Home;
+		}
+		return (
+			<div>
+				<Child />
+			</div>
+			);
 
-module.exports = App;
+	}
+});
+
+function render(){
+	var route = win.location.hash.substr(1);
+	React.render(<App route={route} />, document.getElementById('app'));
+}
+
+win.addEventListener('hashchange', render);
+render();
+}(window));
+
+React.render(<App />, document.getElementById('app'));
